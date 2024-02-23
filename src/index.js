3e884508b8594fd015f3c46204577a4249e9da9b
Vishal Prakash <prakashvishal93@Vishals-MacBook-Air-3.local>
2024-02-24 02:31:04 +0530

    setup body-parser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
   // Create the express object
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
-    console.log(process.env);
   });
 };
 
